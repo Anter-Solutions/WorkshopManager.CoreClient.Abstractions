29083291ca99d6237a61556310cee1f8aacc320f
vladimirmazarakis <87600021+vladimirmazarakis@users.noreply.github.com>
2022-11-18 12:02:17 +0200

    init

---
+++ Providers/ServerAPIClientProviderBase.cs
@@ -0,0 +1,21 @@
+﻿namespace WorkshopManager.CoreClient.Abstractions.Providers
+{
+    /// <summary>
+    /// Provides HttpClient for Server API requests.
+    /// </summary>
+    public abstract class ServerAPIClientProviderBase
+    {
+        protected HttpClient HttpClient { get; init; }
+
+        public ServerAPIClientProviderBase(HttpClient httpClient)
+        {
+            HttpClient = httpClient;
+        }
+
+        /// <summary>
+        /// Returns HttpClient for requests.
+        /// </summary>
+        /// <returns>HttpClient</returns>
+        public abstract HttpClient GetClient();
+    }
+}
---
+++ Providers/WS/IWSSettingsProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkshopManager.Common.WS;
+
+namespace WorkshopManager.CoreClient.Abstractions.Providers.WS
+{
+    public interface IWSSettingsProvider
+    {
+        Task<WSSettings?> GetWSSettings();
+    }
+}
